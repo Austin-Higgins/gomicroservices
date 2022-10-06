cdc4b4c0fbbf2d634e3b4e1576634e973612dbfa
Austin Higgins <ahiggins.dev@gmail.com>
2022-10-06 17:04:57 -0400

    imported a few packages, and added functionality.

--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"net/http"
 	"log"
+	"fmt"
+	"io"
 )
 
-
-func main(){
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request){
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request){
 		log.Println("Hello World")
+		d, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw,"Hello %s", d)
 	})
+
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request){
 		log.Println("Goodbye World")
 	})
+
 	http.ListenAndServe(":4040", nil)
 }
